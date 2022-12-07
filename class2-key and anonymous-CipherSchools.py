49990347d3c3303a028575ec990aa5bbd83f1117
HereIsAbhi <kabhinav201412@gmail.com>
2022-12-07 22:16:39 +0530

    function concepts

---
+++ class2-key and anonymous-CipherSchools.py
@@ -0,0 +1,22 @@
+# python is a dynamic lang i.e uses runtime command
+
+# def func(a,b,c,*d):
+#     print(a)
+#     print(b)
+#     print(c)
+#     print(d)
+# func(1,2,3,4,5,6,)
+
+# def func(a,b,*c,d):
+#     print(a)
+#     print(b)
+#     print(c)
+#     print(d)
+# func(1,2,3,4,5,d=6)
+
+def func(**k):
+    print(k)
+
+func(name="jatin")    
+
+
