4a8062f8578ccf7aaddf1a7df19ca5b8090e4c8c
HereIsAbhi <kabhinav201412@gmail.com>
2022-12-07 20:22:52 +0530

    lectures

---
+++ class1.py
@@ -0,0 +1,17 @@
+# def fn is used decrease redundancy
+
+# def repeat():
+#     for _ in range(3):
+#         print("loading","*"*(_+1))
+
+# repeat()
+
+# def repeat(name="shiva",n=3):
+#     for i in range(n):
+#         print("my name is",name)
+# repeat()
+
+def repeat(name,n=3):
+    for i in range(n):
+        print("my name is",name)
+repeat("abhi")
