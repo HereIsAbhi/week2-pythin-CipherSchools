a668b1666df88ebc76027e56d395897400527009
HereIsAbhi <kabhinav201412@gmail.com>
2022-12-08 14:30:32 +0530

    new added

---
+++ class3-lambda-CipherShools.py
@@ -0,0 +1,22 @@
+# names= ["jatin","saransh","shubham","samarth"]
+# # for name in names:
+# #     print(name)
+
+# # for name in enumerate(names):
+# #     print(name)
+# for a in enumerate(names):
+#     print(a[0],a[1])
+
+a= 9
+b=5
+# a= a^b
+# b=a^b
+# a=a^b
+# print(a,b)
+# a= a+b
+# b= a-b
+# a= a-b
+# print(a,b)
+
+a,b=b,a
+print(a,b)
