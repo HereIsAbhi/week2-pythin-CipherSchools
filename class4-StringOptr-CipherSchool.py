5e25284ff06b908f27b81bac0669e44209d3b5c9
HereIsAbhi <kabhinav201412@gmail.com>
2022-12-11 20:13:49 +0530

    string formatting

---
+++ class4-StringOptr-CipherSchool.py
@@ -0,0 +1,12 @@
+a=5
+# print("value of a is %d" %(a))
+# print("value of a is {}".format(a))
+# print("   abhi".strip())
+# print("abhinav".replace("a","k"))
+# print("abhinav".count("a"))
+print("1,23,3,4".split(","))
+
+name= "jatin katyal"
+company="shutti"
+print("hello {} welcome to my {}".format(name,company))
+print("hello {name} welcome to my {company}".format(name=name,company=company))
